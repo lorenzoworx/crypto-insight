48142c2d44d1013a96ca18aab59ce327cf45de28
Lorenzo <107998561+lorenzoworx@users.noreply.github.com>
2023-06-27 22:06:21 +0100

    ADD crypto reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import cryptoReducer from './crypto/cryptoCurrencySlice';
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    crypto: cryptoReducer,
+  },
 });
 
 export default store;
